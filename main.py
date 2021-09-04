743c5b5028eaeecc94a4e44f1689d22aab567592
singein <singein@outlook.com>
2021-09-04 22:16:43 +0800

    change meta_reflex.py

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import random
 
 def auto_commit_job():
     print("==> Auto commit job start...")
-    codes = open("autocommit.py", encoding='utf-8').read()
-    codes = codes.replace('\n', random.randint(1, 10) * '\n')
     with open('autocommit.py', 'w',encoding='utf-8') as f:
-        f.write(codes)
+        f.write(random.randint(1, 1000) * random.choice('abcdefghijk1235546'))
     import os
     os.system("git add .")
     
